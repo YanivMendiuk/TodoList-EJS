a61c67a9f9d3a63911a098890f8953b83111ea5f
Yaniv Mendiuk <yanivmendiuk@gmail.com>
2020-03-29 01:16:56 +0300

    just finish lesson 281

--- app.js
+++ app.js
@@ -1,25 +1,33 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const request = require("request");
+const https = require("https");
 
 const app = express();
 
+var items = [];
+
 app.set("view engine", "ejs");
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
 
 app.get("/", function(req, res) {
   var today = new Date();
-  var currentDay = today.getDay;
-  var weekdays = [
-    "Sunday",
-    "Monday",
-    "Tuesday",
-    "Wednesday",
-    "Thursday",
-    "Friday",
-    "Saturday"
-  ];
-  var day = weekdays[3];
+  var options = {
+    weekday: "long",
+    day: "numeric",
+    month: "long"
+  };
+  var day = today.toLocaleDateString("en-US", options);
 
-  res.render("list", { kindOfDay: day });
+  res.render("list", { kindOfDay: day, newListItems: items });
+});
+
+app.post("/", function(req, res) {
+  var item = req.body.newItem;
+  items.push(item);
+  res.redirect("/");
 });
 
 app.listen(3000, function() {
